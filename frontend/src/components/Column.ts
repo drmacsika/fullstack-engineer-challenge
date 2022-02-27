3cf56056e77dd21ab6327aaa7254a487953a7c87
drmacsika <drmacsika@gmail.com>
2022-02-27 11:29:06 +0100

    Add column definition schema

---
+++ frontend/src/components/Column.ts
@@ -0,0 +1,84 @@
+
+/**
+ * Define the column headings for the table
+ * 
+ */
+export const COLUMNS = [
+  {
+    Header: 'Customer',
+    accessor: 'customer'
+  },
+  {
+    Header: 'Provider',
+    accessor: 'provider'
+  },
+  {
+    Header: 'Insurance Type',
+    accessor: 'insurance_type'
+  },
+  {
+    Header: 'Status',
+    accessor: 'status'
+  },
+  {
+    Header: 'Policy Number',
+    accessor: 'policy_number'
+  },
+  {
+    Header: 'Start Date',
+    accessor: 'start_date'
+  },
+  {
+    Header: 'End Date',
+    accessor: 'end_date'
+  },
+  {
+    Header: 'Created At',
+    accessor: 'created_at'
+  },
+]
+
+// export const GROUPED_COLUMNS = [
+//   {
+//     Header: 'Id',
+//     Footer: 'Id',
+//     accessor: 'id'
+//   },
+//   {
+//     Header: 'Name',
+//     Footer: 'Name',
+//     columns: [
+//       {
+//         Header: 'First Name',
+//         Footer: 'First Name',
+//         accessor: 'first_name'
+//       },
+//       {
+//         Header: 'Last Name',
+//         Footer: 'Last Name',
+//         accessor: 'last_name'
+//       }
+//     ]
+//   },
+//   {
+//     Header: 'Info',
+//     Footer: 'Info',
+//     columns: [
+//       {
+//         Header: 'Date of Birth',
+//         Footer: 'Date of Birth',
+//         accessor: 'date_of_birth'
+//       },
+//       {
+//         Header: 'Country',
+//         Footer: 'Country',
+//         accessor: 'country'
+//       },
+//       {
+//         Header: 'Phone',
+//         Footer: 'Phone',
+//         accessor: 'phone'
+//       }
+//     ]
+//   }
+// ]
