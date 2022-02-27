5e23c3b2cae748ab4ecdea363c2c67fe629421df
drmacsika <drmacsika@gmail.com>
2022-02-27 01:42:33 +0100

    Add Apollo client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import "./index.css";
+import PolicyTable from "./components/PolicyTable";
+import { useQuery } from '@apollo/client';
+import { POLICY } from './api/queries';
 
-function App() {
-  return <h1 className="text-3xl">Fullstack code challenge</h1>;
+const App = () => {
+    const { loading, error, data } = useQuery(POLICY);
+    if (loading) return <>"Loading..."</>;
+    if (error) return <>`Error! ${error.message}`</>;
+    console.log(JSON.stringify(data));
+    return (
+        <>
+          <div className="container mx-auto px-8 overflow-hidden py-8 pb-20">
+            <h1 className="text-3xl text-center py-2 pt-8">Insurance Policies</h1>
+              <PolicyTable policies={ data?.policies } />
+          </div>
+        </>
+    )
 }
 
 export default App;
