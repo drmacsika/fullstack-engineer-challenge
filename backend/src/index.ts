c5565874b5d6c34817a76292cb71bcd8bc38d7a3
drmacsika <drmacsika@gmail.com>
2022-03-01 23:25:08 +0100

    Add mongo db server.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,25 @@
 import { ApolloServer } from "apollo-server";
-const { typeDefs, dateScalar } = require("./schema");
-import policies_data from "./mock_data/mock_data";
-
-// This resolver retrieves policies from the mock_data or firestore db
-// depending on what we pass to it.
-// It resolves the custom date type going from the firestore to the graphql
-// from graphql to the client and vice versa.
-const resolvers = {
-  Date: dateScalar,
-  Query: {
-    policies: () => policies_data,
-  },
-};
+const { typeDefs } = require("./schema");
+import resolvers from "./resolvers";
+const mongoose = require('mongoose');
+const { MONGODB } = require('./config/config.ts');
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+// // MongoDB connection
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+      console.log('MongoDB Connected');
+      return server.listen({ port: 4000 });
+  })
+  .then((res: any) => {
+    console.log(`Server running at ${res.url}`);
 });
+
+// The `listen` method launches a web server.
+// server.listen().then(({ url }) => {
+//     console.log(`🚀  Server ready at ${url}`);
+// });
