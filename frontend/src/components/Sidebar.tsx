21ca4693b228b35778ea193b96e144f6e1b51b55
drmacsika <drmacsika@gmail.com>
2022-03-01 23:09:09 +0100

    Add sidebar.

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,48 @@
+import Dashboard from "./Dashboard";
+import Profile from "./Profile";
+import { Link } from "react-router-dom";
+
+
+const Sidebar = () => {
+    return (
+        <>
+            <aside className="aside is-placed-left is-expanded">
+                <div className="aside-tools">
+                    <div>
+                        Feather Insurance <b className="font-black">Admin</b>
+                    </div>
+                </div>
+                <div className="menu is-menu-main">
+                    <ul className="menu-list">
+                        <li className="--set-active-index-html">
+                            <a href="index.html">
+                            <span className="icon"><i className="mdi mdi-desktop-mac"></i></span>
+                            <span className="menu-item-label">
+                                <Link to="/dashboard">Dashboard</Link>
+                            </span>
+                            </a>
+                        </li>
+                        <li className="--set-active-index-html">
+                            <a href="index.html">
+                            <span className="icon"><i className="mdi mdi-desktop-mac"></i></span>
+                            <span className="menu-item-label">
+                                <Link to="/profile">Profile</Link>
+                            </span>
+                            </a>
+                        </li>
+                        <li className="--set-active-index-html">
+                            <a href="index.html">
+                            <span className="icon"><i className="mdi mdi-desktop-mac"></i></span>
+                            <span className="menu-item-label">
+                                <Link to="/create-admin">Create New Admin</Link>
+                            </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                </aside>
+        </>
+    )
+}
+
+export default Sidebar
