9aa5375abafe6d15af73e14d595bd3ae0becd8e3
drmacsika <drmacsika@gmail.com>
2022-03-01 23:11:31 +0100

    Add backend key configurations.

---
+++ backend/src/config/config.ts
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: "mongodb+srv://drmacsika:M7uUD2vgPXc0z4YR@cluster0.dgqsu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    SECRET_KEY: "76q3uydgutcyfxgwuwedcdq6536e7y9odb1qrwx76tguysdc/wAEID';WETY"
+}
