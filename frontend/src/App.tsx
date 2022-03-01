75fd7237bdc6da30e1fb735681041c084ef4ce91
drmacsika <drmacsika@gmail.com>
2022-03-01 23:26:13 +0100

    Add router and state provider.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,22 @@
 import "./index.css";
-import PolicyTable from "./components/PolicyTable";
-import { useQuery } from '@apollo/client';
-import { POLICY } from './api/queries';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import Register from './components/Register';
+import  { AuthProvider } from "./state/auth"
 
 const App = () => {
-    const { loading, error, data } = useQuery(POLICY);
-    if (loading) return <>"Loading..."</>;
-    if (error) return <>`Error! ${error.message}`</>;
-    console.log(JSON.stringify(data));
     return (
-        <>
-          <div className="container mx-auto px-8 overflow-hidden py-8 pb-20">
-            <h1 className="text-3xl text-center py-2 pt-8">Insurance Policies</h1>
-              <PolicyTable policies={ data?.policies } />
-          </div>
-        </>
-    )
+        <AuthProvider>
+          <Routes>
+            <Route path='/' element={<Login/>}></Route>
+            <Route path='/dashboard' element={<Dashboard/>}></Route>
+            <Route path='/profile' element={<Profile/>}></Route>
+            <Route path='/create-admin' element={<Register/>}></Route>
+          </Routes>
+        </AuthProvider>
+    );
 }
 
 export default App;
